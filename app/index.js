783f7451cf06e6f8bf1d5d542d275a7ad734114f
Zoe Ames <zoeames@gmail.com>
2014-07-29 12:59:08 -0500

    initial functions added

---
+++ app/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res){
+      res.render('home');
+});
+
+
+
+
+app.listen(process.env.PORT, function(){
+      console.log('hellooooooo...');
+});
+
